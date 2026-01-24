aa9ea6ae8efd6255cfaa0a633d10920d36e064fd
castilloderekandre <castilloderekandre@gmail.com>
2026-01-24 13:44:07 -0600

    Unsafe keyword

---
+++ ascii_mapper/ImagePatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ascii_mapper
+{
+    internal class ImagePatch
+    {
+        unsafe struct FeatureVector
+        {
+            public float mean;
+            public float std;
+            public float edgeDensity;
+            public float gradientMean;
+            public float gradientStd;
+
+            public fixed float orientationHistogram[8];
+
+            public float fill;
+            public byte dominant;
+        }
+        public ImagePatch()
+        {
+
+        }
+    }
+}

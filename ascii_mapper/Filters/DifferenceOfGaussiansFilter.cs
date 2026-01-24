bb709821d8669d51e95dc9887534650a59879be8
castilloderekandre <castilloderekandre@gmail.com>
2026-01-24 14:26:14 -0600

    Added missing interface partial implementations. Added proper file structure

--- ascii_mapper/Filters/DifferenceOfGaussiansFilter.cs
+++ ascii_mapper/Filters/DifferenceOfGaussiansFilter.cs
@@ -7,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace ascii_mapper.Filters
 {
-    internal class DifferenceOfGaussiansFilter
+    internal class DifferenceOfGaussiansFilter : IFilter
     {
+        public Bitmap Apply(Bitmap image)
+        {
+            // Placeholder implementation for Difference of Gaussians filter
+            // In a real implementation, you would apply the DoG algorithm here
+            return image;
+        }
     }
 }
